592a6789cf2023eb83b2501986519f58e5fddb34
Yaz <yasmineoshaugh@gmail.com>
2020-05-18 23:24:37 -0700

    working state machine

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import TrafficLight from './components/TrafficLight';
-import { service } from './state-machines/LightMachine';
+import { lightMachine, service } from './state-machines/LightMachine';
 import './App.css';
 
 // Constants to use when changing the light
@@ -39,6 +39,19 @@ const App = () => {
       console.log(state);
       // Your code here to change lightStatus when the 
       //   state of the state machine changes
+      switch(state.value) {
+        case "red":
+          setLightStatus(GREEN_LIGHT);
+          break;
+        case "yellow":
+          setLightStatus(RED_LIGHT);
+          break;
+        case "green":
+          setLightStatus(YELLOW_LIGHT);
+          break;
+        default:
+          console.log("invalid state!");
+      }
 
     });
   }, [])
